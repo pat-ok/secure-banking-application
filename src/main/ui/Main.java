a9856036c6550ba8208da6322f74242dadd41788
pat <pat.liang@alum.utoronto.ca>
2022-04-14 20:07:43 -0700

    Broke main method

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -3,7 +3,7 @@ package ui;
 import ui.pages.BankingApp;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void mainn(String[] args) {
         new BankingApp();
     }
 }
